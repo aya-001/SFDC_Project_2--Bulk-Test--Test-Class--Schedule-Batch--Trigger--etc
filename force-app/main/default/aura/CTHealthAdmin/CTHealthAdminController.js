b9c2d7e7a712f17a499e88dd1a67371be5612f2e
akaya1023 <an7099265@gmail.com>
2023-03-23 20:37:43 +0900

    Added Component

---
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -0,0 +1,11 @@
+({
+    handleSelect : function(component, event, helper) {
+        const selectedTabId =  event.getParam("id");
+        if(selectedTabId === "person"){
+            component.set("v.headerTitle", "Person View");
+        }else{
+            component.set("v.headerTitle", "Location View");
+        }
+        component.set("v.scope", selectHandleTabId);
+    }
+})
