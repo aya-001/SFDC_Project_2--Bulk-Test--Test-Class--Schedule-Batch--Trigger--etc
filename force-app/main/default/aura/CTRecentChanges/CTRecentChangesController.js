e4015358d77ec1b937e8d05ab2032ce90c87a5e4
akaya1023 <an7099265@gmail.com>
2023-03-26 00:49:15 +0900

    added search function

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -23,6 +23,21 @@
             ]);
         }
         helper.fetchRecentHealthChanges(component);
+    },
+
+    handleKeyUp : function(component,event, helper) {
+        const isEnterKey = event.keyCode === 13;
+        var queryTerm = component.find("enter-search").get("v.value");
+
+        //if search term is blank
+        if(!queryTerm){
+            component.set("v.data", component.get("v.initialResponse"));
+        }
+        if (isEnterKey) {
+            //show spiner
+            component.set("v.issearching", true);
+            helper.searchRecord(component, queryTerm);
+        }
 
     }
 });
